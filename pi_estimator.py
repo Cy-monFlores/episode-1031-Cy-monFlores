124fa1e486e8eb154adb8ce9f67cfe75202248c1
Cy-monFlores <simonflores5117@gmail.com>
2025-11-01 15:10:18 -0700

    fully usable program now!

--- pi_estimator.py
+++ pi_estimator.py
@@ -1,10 +1,14 @@
 import sys
-
+import random
 total_darts = 9
 if len(sys.argv) > 1:
     total_darts = int(sys.argv[1])
 
 in_circle = 0
 for i in range(total_darts):
-
-    print(f"throwing dart#{i}")
+    dart_x = random.random()
+    dart_y = random.random()
+    
+    if dart_x*dart_x + dart_y*dart_y <= 1:
+        in_circle += 1
+print(f"in circle: {in_circle} out of {total_darts}")
